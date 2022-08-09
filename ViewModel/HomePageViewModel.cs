d92085e6104152801e2650f913e2f9296b823476
Santosh Dahal <santoshdahal@bajratechnologies.com>
2022-08-09 22:46:57 +0545

    Add project files.

---
+++ ViewModel/HomePageViewModel.cs
@@ -0,0 +1,39 @@
+﻿
+
+using EcommerceMAUI.Model;
+using System.Collections.ObjectModel;
+
+namespace EcommerceMAUI.ViewModel
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+
+        public ObservableCollection<CategoriesModel> _CategoriesDataList = new ObservableCollection<CategoriesModel>();
+        public ObservableCollection<CategoriesModel> CategoriesDataList
+        {
+            get
+            {
+                return _CategoriesDataList;
+            }
+            set
+            {
+                _CategoriesDataList = value;
+                OnPropertyChanged("CategoriesDataList");
+            }
+        }
+        public HomePageViewModel()
+        {
+            PopulateData();
+        }
+
+        void PopulateData()
+        {
+            CategoriesDataList.Clear();
+            CategoriesDataList.Add(new CategoriesModel() { CategoryID=1, CategoryName="Men", Icon= "\ufb22" });
+            CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Women", Icon= "\ufb23" });
+            CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Devices", Icon= "\uf322" });
+            CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Gadgets", Icon= "\uf2cb" });
+            CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Games", Icon= "\uf5ba" });
+        }
+    }
+}
---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+using EcommerceMAUI.ViewModel;
+
+namespace EcommerceMAUI.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+        BindingContext = new HomePageViewModel();
+    }
+}
