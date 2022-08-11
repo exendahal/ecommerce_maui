467c83ffdb0a4e52fefc0311fa6550b5354a33ed
Santosh Dahal <santoshdahal@bajratechnologies.com>
2022-08-11 22:21:37 +0545

    feat: COmpletion of homepage design

---
+++ Model/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceMAUI.Model
+{
+    public class ProductModel
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string BrandName { get; set; }
+        public string Price { get; set; }
+        public string Details { get; set; }
+    }
+}
--- ViewModel/HomePageViewModel.cs
+++ ViewModel/HomePageViewModel.cs
@@ -21,6 +21,34 @@ namespace EcommerceMAUI.ViewModel
                 OnPropertyChanged("CategoriesDataList");
             }
         }
+
+        public ObservableCollection<ProductModel> _BestSellingDataList = new ObservableCollection<ProductModel>();
+        public ObservableCollection<ProductModel> BestSellingDataList
+        {
+            get
+            {
+                return _BestSellingDataList;
+            }
+            set
+            {
+                _BestSellingDataList = value;
+                OnPropertyChanged("BestSellingDataList");
+            }
+        }
+
+        public ObservableCollection<ProductModel> _FeaturedBrandsDataList = new ObservableCollection<ProductModel>();
+        public ObservableCollection<ProductModel> FeaturedBrandsDataList
+        {
+            get
+            {
+                return _FeaturedBrandsDataList;
+            }
+            set
+            {
+                _FeaturedBrandsDataList = value;
+                OnPropertyChanged("FeaturedBrandsDataList");
+            }
+        }
         public HomePageViewModel()
         {
             PopulateData();
@@ -34,6 +62,18 @@ namespace EcommerceMAUI.ViewModel
             CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Devices", Icon= "\uf322" });
             CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Gadgets", Icon= "\uf2cb" });
             CategoriesDataList.Add(new CategoriesModel() { CategoryID=2, CategoryName="Games", Icon= "\uf5ba" });
+
+            BestSellingDataList.Clear();
+            BestSellingDataList.Add(new ProductModel() { Name = "BeoPlay Speaker", BrandName = "Bang and Olufsen", Price = "$755",ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image1.png" });
+            BestSellingDataList.Add(new ProductModel() { Name = "Leather Wristwatch", BrandName = "Tag Heuer", Price = "$450",ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image2.png" });
+            BestSellingDataList.Add(new ProductModel() { Name = "Smart Bluetooth Speaker", BrandName = "Google LLC", Price = "$900",ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image3.png" });
+            BestSellingDataList.Add(new ProductModel() { Name = "Smart Luggage", BrandName = "Smart Inc", Price = "$1200",ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Image4.png" });
+
+            FeaturedBrandsDataList.Clear();
+            FeaturedBrandsDataList.Add(new ProductModel() { BrandName = "B&o", Details = "5693 Products", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Icon_Bo.png" });
+            FeaturedBrandsDataList.Add(new ProductModel() { BrandName = "Beats", Details = "1124 Products", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/beats.png" });
+            FeaturedBrandsDataList.Add(new ProductModel() { BrandName = "Apple Inc", Details = "5693 Products", ImageUrl = "https://raw.githubusercontent.com/exendahal/ecommerceXF/master/eCommerce/eCommerce.Android/Resources/drawable/Icon_Apple.png" });
+           
         }
     }
 }
